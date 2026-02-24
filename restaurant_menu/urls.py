d977b074c9fa5526a6b1d311f93e0af428736982
Ali Aljanabi <114556000+alialjanabi12@users.noreply.github.com>
2026-02-23 22:07:00 -0800

    Adding Dynamic Links

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.MenuListView.as_view(), name="home"),
-    path("", views.MenuItemDetailView.as_view(), name="index"),
+    path("menu_item/<int:pk>/", views.MenuItemDetailView.as_view(), name="menu_item"),
 ]
